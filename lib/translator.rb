b124760eec9f35ef31e6b6e618729f6b9db00155
Angela Lo <angelawenlo@gmail.com>
2019-06-03 13:17:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ def get_japanese_emoticon(file, emoticons)
   # code goes here
   load_library = load_library(file)
   load_library["get_emoticon"].each do |english, japanese|
+    binding.pry
     if english.include?(emoticon)
       puts japanese
     end
