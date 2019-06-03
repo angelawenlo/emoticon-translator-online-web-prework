f0301a5392b3f34a80385d13b060b08376f854cc
Angela Lo <angelawenlo@gmail.com>
2019-06-03 13:27:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file, emoticons)
   load_library = load_library(file)
   load_library["get_emoticon"].each do |english, japanese|
     if english == emoticons
-      return japanese
+     japanese
     else
        "Sorry, that emoticon was not found"
     end
