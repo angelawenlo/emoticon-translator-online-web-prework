13f208983d0d1de6135d4cd84e9e73987908c78d
Angela Lo <angelawenlo@gmail.com>
2019-06-03 13:21:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file, emoticons)
   load_library = load_library(file)
   load_library["get_emoticon"].each do |english, japanese|
     if english.include?(emoticons)
-       japanese
+      return japanese
     end
   end
 end
