88beed775f83f800fe205a77d464f86f1fc2f978
Angela Lo <angelawenlo@gmail.com>
2019-06-03 13:31:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ def get_japanese_emoticon(file, emoticons)
   # code goes here
   load_library = load_library(file)
   result = load_library["get_emoticon"][emoticon]
+  binding.pry
   load_library.each do |english, japanese|
     if english == emoticons
       return japanese
